17e043ed0cb0571467d8edbe8944dea79738d6f7
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2025-10-06 11:35:02 -0300

    button component created & Page component using Button component

--- exercise06-react.jsx
+++ exercise06-react.jsx
@@ -1,11 +1,18 @@
 /**
  * Melhore o código abaixo
-*/
+ */
+import React from "react";
+
+//Criei um componente para o botão, dentro do próprio componente. Pois é só ele que usará esse btn
+export default function Button({value}){
+	return <button className="btn-primary">{value}</button>;
+}
+
 export default function Page() {
-	return (
-		<>
-			<button className='btn-primary'>Salvar</button>
-			<button className='btn-primary'>Cancelar</button>
-		</>
-	)
+  return (
+    <>
+      <Button value="Salvar" />
+      <Button value="Cancelar" />
+    </>
+  );
 }
