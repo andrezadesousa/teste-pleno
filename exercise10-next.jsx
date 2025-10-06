2e58f71dbef25e3097ee35a1fa2b8ecb3b87f30b
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2025-10-06 11:35:42 -0300

    Dashboard using box and title components

--- exercise10-next.jsx
+++ exercise10-next.jsx
@@ -4,27 +4,30 @@
  * reutilizáveis para cada seção (Summary, Graph, Table) e aplique boas práticas de
  * composição visual.
  */
+import Title from "./components/title";
+import Box from "./components/box";
 export default function Dashboard() {
-	return (
-		<div className='dashaboard'>
-			<div className='box'>
-				<h2>Resumo</h2>
-				<p>Dados resumidos...</p>
-			</div>
-			<div className='box'>
-				<h2>Gráfico</h2>
-				<canvas id='graph'></canvas>
-			</div>
-			<div className='box'>
-				<h2>Histórico</h2>
-				<table>
-					<tbody>
-						<tr>
-							<td>Carregando...</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	)
+  return (
+    <div className="dashaboard">
+      <Box>
+        <div className="box">
+          <Title value="Resumo" />
+          <p>Dados resumidos...</p>
+        </div>
+      </Box>
+      <Box>
+        <Title value="Gráfico" />
+        <canvas id="graph"></canvas>
+      </Box>
+      <Box>
+        <table>
+          <tbody>
+            <tr>
+              <td>Carregando...</td>
+            </tr>
+          </tbody>
+        </table>
+      </Box>
+    </div>
+  );
 }
